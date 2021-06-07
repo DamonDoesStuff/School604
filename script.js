ad6de4837cbb6cabd192f10fc309e9123920763e
Damon Rochon <>
2021-06-07 14:10:44 +0000

    Help

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let shortbase = 0
 let longbase = 0
 let height = 0
 let answer = 0
-let two = 2
+
 document.getElementById('button').addEventListener('click', multiplication)
 
-function multiplication() {
+function multiplication () {
   shortbase = document.getElementById('box1').value
   longbase = document.getElementById('box2').value
   height = document.getElementById('box3').value
@@ -13,6 +13,6 @@ function multiplication() {
   shortbase = parseInt(shortbase)
   longbase = parseInt(longbase)
   height = parseInt(height)
-  answer = (shortbase + longbase) / two * height
+  answer = (shortbase + longbase) / 2 * height
   alert(answer)
 }
